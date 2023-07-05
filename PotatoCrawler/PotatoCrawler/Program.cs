326405f71cdd0dd5ef65e45129b82bc75525edcc
nvor <nickbaripour@gmail.com>
2023-07-05 14:46:37 -0400

    init playwright project

---
+++ PotatoCrawler/PotatoCrawler/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+
+namespace PotatoCrawler
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync();
+
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://reddit.com");
+            //await page.ScreenshotAsync(new() { Path = "screenshot.png" });
+        }
+    }
+}
